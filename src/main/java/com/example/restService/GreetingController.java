2b53f01fa2b989a1e721da8eb1ccb9fd4868051c
Dieginxe <cesardiego81@gmail.com>
2023-06-27 17:06:06 -0500

    controller

--- src/main/java/com/example/restService/GreetingController.java
+++ src/main/java/com/example/restService/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/saludo")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "Mundo") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
